67519f126e6cf691e29e6b433de7b5788b065959
Michael Kamb <michael.kamb@yahoo.com>
2025-05-08 22:07:49 -0500

    Fix change_post_date.rb util
    Remove accidentally duplicated code text.

--- utils/change_post_date.rb
+++ utils/change_post_date.rb
@@ -51,34 +51,3 @@ if ARGV.size != 2
 end
 
 update_blog_post_date(ARGV[0], ARGV[1])
-
-require "fileutils"
-
-def update_blog_post_date(old_filename, new_date)
-  unless new_date.match?(/^\d{4}-\d{2}-\d{2}$/)
-    puts "Error: Date must be in YYYY-MM-DD format"
-    exit 1
-  end
-
-  # Extract base name and handle multiple extensions
-  base = old_filename.sub(/(-\d{4}-\d{2}-\d{2})(\..+)/, "")
-  ext = old_filename.match(/(-\d{4}-\d{2}-\d{2})(\..+)/)[2]
-  new_filename = "#{base}-#{new_date}#{ext}"
-
-  # Process content
-  content = File.read(old_filename)
-  updated_content = content.gsub(/^(date: )\d{4}-\d{2}-\d{2}/, "\\1#{new_date}")
-
-  # Rename and update
-  FileUtils.mv(old_filename, new_filename)
-  File.write(new_filename, updated_content)
-
-  puts "Updated: #{old_filename} → #{new_filename}"
-end
-
-if ARGV.size != 2
-  puts "Usage: #{$0} <filename> <new-date>"
-  exit 1
-end
-
-update_blog_post_date(ARGV[0], ARGV[1])
